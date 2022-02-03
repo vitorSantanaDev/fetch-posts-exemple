3997cdfd2f167a2425ebc681c689f42956c20bf1
vitorSantanaDev <vitorsantana.developer@gmail.com>
2022-02-03 08:03:24 -0300

    🚚 Change the name and path of the Home component

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './styles/globaStyles.css';
+import Home from './templates/Home/Home';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Home />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
