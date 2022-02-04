9394c88207ef5fb19870660e8229dc66adc471c3
vitorSantanaDev <vitorsantana.developer@gmail.com>
2022-02-03 22:46:48 -0300

    🎨 Eslint configuration, editorconfig and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,12 +4,7 @@ module.exports = {
     es2021: true,
     jest: true,
   },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
-    'plugin:prettier/recommended',
-  ],
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:react-hooks/recommended', 'plugin:prettier/recommended'],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
@@ -25,6 +20,5 @@ module.exports = {
   },
   rules: {
     'react/react-in-jsx-scope': 'off',
-    'no-unused-vars': 'off',
   },
 };
