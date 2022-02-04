24d18ad0bb55304511cdd3f4a1c5a822e6dcb873
vitorSantanaDev <vitorsantana.developer@gmail.com>
2022-02-03 22:48:53 -0300

    🚩 Migrating our Home template to React Hooks

--- src/templates/Home/Home.jsx
+++ src/templates/Home/Home.jsx
@@ -1,51 +1,60 @@
-import { Component } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import './styles.css';
+import { loadPosts } from '../../utils/loadPosts.js';
 import Container from '../../components/Container';
 import Posts from '../../components/Posts';
-import { loadPosts } from '../../utils/loadPosts.js';
 import Button from '../../components/Button';
+import Input from '../../components/Input';
 
-class Home extends Component {
-  state = {
-    page: 0,
-    postsPerPage: 3,
-    allPosts: [],
-    posts: [],
-  };
+const Home = () => {
+  const [page, setPage] = useState(0);
+  const [postsPerPage] = useState(3);
+  const [allPosts, setAllPosts] = useState([]);
+  const [posts, setPosts] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
 
-  loadMorePosts = () => {
-    const { page, postsPerPage, allPosts, posts } = this.state;
+  const loadMorePosts = () => {
     const nextPage = page + postsPerPage;
     const nextPosts = allPosts.slice(nextPage, nextPage + postsPerPage);
 
     posts.push(...nextPosts);
-    this.setState({ posts, page: nextPage });
+    setPosts(posts);
+    setPage(nextPage);
   };
 
-  loadPostsInTheHome = async () => {
-    const { page, postsPerPage } = this.state;
+  const loadPostsInTheHome = useCallback(async (page, postsPerPage) => {
     const postsAndPhotos = await loadPosts();
+    setPosts(postsAndPhotos.slice(page, postsPerPage));
+    setAllPosts(postsAndPhotos);
+  }, []);
 
-    this.setState({
-      posts: postsAndPhotos.slice(page, postsPerPage),
-      allPosts: postsAndPhotos,
-    });
+  const handleChangeInputField = ({ target }) => {
+    setSearchValue(target.value);
   };
 
-  componentDidMount() {
-    this.loadPostsInTheHome();
-  }
+  useEffect(() => {
+    loadPostsInTheHome(0, postsPerPage);
+  }, [loadPostsInTheHome, postsPerPage]);
 
-  render() {
-    const { page, postsPerPage, allPosts, posts } = this.state;
-    const noMorePosts = page + postsPerPage >= allPosts.length;
-    return (
-      <Container>
-        <Posts posts={posts} />
-        <Button disabled={noMorePosts} onClick={this.loadMorePosts} text="Load More Posts" />
-      </Container>
-    );
-  }
-}
+  const noMorePosts = page + postsPerPage >= allPosts.length;
+  const filteredPosts = searchValue
+    ? allPosts.filter((post) => {
+        return post.title.toLowerCase().includes(searchValue.toLowerCase());
+      })
+    : posts;
+
+  return (
+    <Container>
+      <div className="searchContainer">
+        {!!searchValue && <h1>Search: {searchValue}</h1>}
+        <Input value={searchValue} onChange={handleChangeInputField} />
+      </div>
+
+      {filteredPosts.length > 0 && <Posts posts={filteredPosts} />}
+      {filteredPosts.length === 0 && <h1>Post não encontrado! :(</h1>}
+      {!searchValue && <Button disabled={noMorePosts} onClick={loadMorePosts} text="Load More Posts" />}
+    </Container>
+  );
+};
 
 export default Home;
