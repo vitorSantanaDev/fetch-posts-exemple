bf6695d3bcc6bdc9bc1545c92713d3ccf6d74c70
vitorSantanaDev <vitorsantana.developer@gmail.com>
2022-02-05 00:10:56 -0300

    ✅ Adding updates to Button component tests

--- src/components/Button/Button.spec.jsx
+++ src/components/Button/Button.spec.jsx
@@ -1,38 +1,38 @@
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import Button from '.';
-
-const mock = {
-  text: 'Load More Posts',
-  onClick: jest.fn(),
-  disabled: false,
-};
+import { buttonProps } from './mock';
 
 describe('<Button/>', () => {
   it('should render the Button with the text "Load More Posts"', () => {
-    render(<Button {...mock} />);
+    render(<Button {...buttonProps} />);
     expect.assertions(1);
     const button = screen.getByRole('button', { name: /load more posts/i });
     expect(button).toBeInTheDocument();
   });
 
   it('should call function on click Button', () => {
-    render(<Button {...mock} />);
+    render(<Button {...buttonProps} />);
     const button = screen.getByRole('button', { name: /load more posts/i });
     userEvent.click(button);
-    expect(mock.onClick).toHaveBeenCalledTimes(1);
+    expect(buttonProps.onClick).toHaveBeenCalledTimes(1);
   });
 
   it('should be disabled when disabled is true', () => {
     const onClick = jest.fn();
-    render(<Button onClick={onClick} disabled={!mock.disabled} text={mock.text} />);
+    render(<Button onClick={onClick} disabled={!buttonProps.disabled} text={buttonProps.text} />);
     const button = screen.getByRole('button', { name: /load more posts/i });
     expect(button).toBeDisabled();
   });
 
   it('should be enabled when disabled is false', () => {
-    render(<Button onClick={mock.onClick} disabled={mock.disabled} text={mock.text} />);
+    render(<Button onClick={buttonProps.onClick} disabled={buttonProps.disabled} text={buttonProps.text} />);
     const button = screen.getByRole('button', { name: /load more posts/i });
     expect(button).toBeEnabled();
+  });
+
+  it('should match snapshot', () => {
+    const { container } = render(<Button {...buttonProps} />);
+    expect(container.firstChild).toMatchSnapshot();
   });
 });
