e8113664542a1f51ac8523e51512677eeafc382b
vitorSantanaDev <vitorsantana.developer@gmail.com>
2022-02-05 13:04:49 -0300

    ✅ Created the Posts component tests

---
+++ src/components/Posts/Posts.spec.jsx
@@ -0,0 +1,17 @@
+import { render, screen } from '@testing-library/react';
+import Posts from '.';
+import { postsProps } from './mock';
+
+describe('<Posts />', () => {
+  it('should render Posts', () => {
+    render(<Posts posts={postsProps} />);
+    expect(screen.getAllByRole('heading', { name: /title/i })).toHaveLength(3);
+    expect(screen.getAllByRole('img', { name: /title/i })).toHaveLength(3);
+    expect(screen.getAllByText(/body/i)).toHaveLength(3);
+  });
+
+  it('should match snapshot', () => {
+    const { container } = render(<Posts posts={postsProps} />);
+    expect(container.firstChild).toMatchSnapshot();
+  });
+});
