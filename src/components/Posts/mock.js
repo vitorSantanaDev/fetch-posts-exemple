774afd4b734970a65cddc414eeb92495f1651d39
vitorSantanaDev <vitorsantana.developer@gmail.com>
2022-02-05 13:04:18 -0300

    ⚡️ Created a mock of the Posts component props

---
+++ src/components/Posts/mock.js
@@ -0,0 +1,5 @@
+export const postsProps = [
+  { id: 1, title: 'Title Post one', body: 'Body text Post one', cover: '/image/image.png' },
+  { id: 2, title: 'Title Post two', body: 'Body text Post two', cover: '/image/image.png' },
+  { id: 3, title: 'Title Post three', body: 'Body text Post three', cover: '/image/image.png' },
+];
