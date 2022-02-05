b0af87201c823ef78ba782a613ba574d748c21b9
vitorSantanaDev <vitorsantana.developer@gmail.com>
2022-02-04 23:32:59 -0300

    ✅ Added update in Button component tests

--- src/components/Button/Button.spec.jsx
+++ src/components/Button/Button.spec.jsx
@@ -2,33 +2,36 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import Button from '.';
 
+const mock = {
+  text: 'Load More Posts',
+  onClick: jest.fn(),
+  disabled: false,
+};
+
 describe('<Button/>', () => {
   it('should render the Button with the text "Load More Posts"', () => {
-    const onClick = jest.fn();
-    render(<Button onClick={onClick} disabled={false} text="Load More Posts" />);
+    render(<Button {...mock} />);
     expect.assertions(1);
     const button = screen.getByRole('button', { name: /load more posts/i });
     expect(button).toBeInTheDocument();
   });
 
   it('should call function on click Button', () => {
-    const onClick = jest.fn();
-    render(<Button onClick={onClick} disabled={false} text="Load More Posts" />);
+    render(<Button {...mock} />);
     const button = screen.getByRole('button', { name: /load more posts/i });
     userEvent.click(button);
-    expect(onClick).toHaveBeenCalledTimes(1);
+    expect(mock.onClick).toHaveBeenCalledTimes(1);
   });
 
   it('should be disabled when disabled is true', () => {
     const onClick = jest.fn();
-    render(<Button onClick={onClick} disabled={true} text="Load More Posts" />);
+    render(<Button onClick={onClick} disabled={!mock.disabled} text={mock.text} />);
     const button = screen.getByRole('button', { name: /load more posts/i });
     expect(button).toBeDisabled();
   });
 
   it('should be enabled when disabled is false', () => {
-    const onClick = jest.fn();
-    render(<Button onClick={onClick} disabled={false} text="Load More Posts" />);
+    render(<Button onClick={mock.onClick} disabled={mock.disabled} text={mock.text} />);
     const button = screen.getByRole('button', { name: /load more posts/i });
     expect(button).toBeEnabled();
   });
